4bd99e44406af52d7dfb30824c805518bb050400
James Stinson-Cerra <jasc5797@yahoo.com>
2018-04-22 01:33:43 -0400

    Test

--- Nodes/SpawnPoint/SpawnPointNode.gd
+++ Nodes/SpawnPoint/SpawnPointNode.gd
@@ -1,13 +1,21 @@
 extends "res://Nodes/TileBasedNode/TileBasedNode.gd"
 
-export (String, "Player", "Enemy") var faction
+#export (String, "Player", "Enemy") var faction
+export (PackedScene) var entity setget set_entity, get_entity
 
 func _ready():
 	if Engine.is_editor_hint():
+		#add_child(entity.instance())
 		show()
 	else:
 		hide()
 	collidable = false
 
-func get_faction():
-	return faction
+#func get_faction():
+#	return faction
+
+func set_entity(value):
+	entity = value
+
+func get_entity():
+	return entity
